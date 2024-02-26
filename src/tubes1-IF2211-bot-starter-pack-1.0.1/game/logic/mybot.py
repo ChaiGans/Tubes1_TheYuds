3bea132551c3de80bbeedbce948cf65c718db248
ChaiGans <elbertchailes888@gmail.com>
2024-02-26 17:48:28 +0700

    refactor: add comments

--- src/tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/mybot.py
+++ src/tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/mybot.py
@@ -8,15 +8,18 @@ from ..util import get_direction, position_equals
 
 
 class MyBot(BaseLogic):
+    # Fungsi Konstruktor
     def __init__(self):
         self.directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         self.goal_position: Optional[Position] = None
         self.current_direction = 0
         self.is_portal_entry = False
-        
+    
+    # Fungsi untuk menghitung jarak dua titik
     def displacement(self, current_position: Position, goal_position: Position):
         return math.sqrt((current_position.x - goal_position.x) ** 2 + (current_position.y - goal_position.y) ** 2)
     
+    # Fungsi untuk menentukan apakah sebuah titik mengandung teleporter atau tidak
     def is_teleporter_position (self, current_position : Position, board: Board):
         list_portal = [x for x in board.game_objects if x.type == "TeleportGameObject"]
         for portal in list_portal:
@@ -24,6 +27,7 @@ class MyBot(BaseLogic):
                 return True
         return False
     
+    # Fungsi untuk menentukan apakah sebuah titik mengandung diamond atau tidak
     def is_diamond_position (self, current_position : Position, board: Board):
         list_diamond = [x for x in board.game_objects if x.type == "DiamondGameObject"]
         for diamond in list_diamond:
@@ -33,6 +37,7 @@ class MyBot(BaseLogic):
                 return True
         return False
     
+    # Fungsi untuk menentukan arah yang dilalui oleh robot
     def possible_direction(self, current_position: Position, board: Board):
         direction_available = []
         
@@ -47,7 +52,7 @@ class MyBot(BaseLogic):
             direction_available.append((0, 1))
         
         return direction_available
-    
+
     def portal_to_diamond_displacement(self, current_position: Position, portal_one_position: Position, portal_two_position: Position, board: Board):
         displacement_bot_to_portal_one = self.displacement(current_position, portal_one_position)
         displacement_bot_to_portal_two = self.displacement(current_position, portal_two_position)
@@ -93,21 +98,29 @@ class MyBot(BaseLogic):
 
         return initial_portal, total_displacement
     
+    # Fungsi untuk mencari posisi portal yang terdapat pada papan
     def find_portal_position(self, board: Board):
         list_portal = [x for x in board.game_objects if x.type == "TeleportGameObject"]
         return list_portal[0].position, list_portal[1].position
 
+    # Fungsi untuk menentukan langkah selanjutnya yang akan dilalui robot
     def next_move(self, board_bot: GameObject, board: Board):
+
+        # Inisialisasi
         props = board_bot.properties
         current_position = board_bot.position
         first_portal_position, second_portal_position = self.find_portal_position(board)
         base = board_bot.properties.base
         time_rem = props.milliseconds_left
+
+        # Status
         print("Current bot position : ", current_position)
         print("First portal position : ", first_portal_position)
         print("Second portal position : ", second_portal_position)
         print("Base : ", base)
         print("DIAMOND : ", props.diamonds)
+
+
         if props.diamonds == 5:
             initial_portal_position, effective_portal_base_displacement = self.portal_to_base_displacement(current_position,first_portal_position, second_portal_position, board_bot)
             if (self.displacement(current_position,base) > effective_portal_base_displacement):
@@ -115,7 +128,6 @@ class MyBot(BaseLogic):
             else:
                 self.goal_position = base
         else:
-            # print(board.game_objects)
             listJarak = []
             for diamond in board.diamonds:
                 print(diamond.position)
@@ -140,6 +152,7 @@ class MyBot(BaseLogic):
                 else:
                     self.goal_position = base
             
+            # Kasus ketika robot baru masuk ke dalam teleporter
             if (self.is_teleporter_position(current_position, board)):
                 direction_available  = self.possible_direction(current_position, board)
                 print("Direction available :", direction_available)
@@ -152,8 +165,8 @@ class MyBot(BaseLogic):
                         else:
                             minDiamond = min(listJarak, key = lambda x: x[1])
                             self.goal_position = minDiamond[0]
-                            
 
+            # Kasus ketika waktu yang tersisa dalam permainan dibawah 10 detik
             if time_rem <= 10000:
                 if(props.diamonds > 1):
                     initial_portal_position, effective_portal_base_displacement = self.portal_to_base_displacement(current_position,first_portal_position, second_portal_position, board_bot)
@@ -162,6 +175,7 @@ class MyBot(BaseLogic):
                     else:
                         self.goal_position = base
 
+        # Pengembalian nilai fungsi
         delta_x, delta_y = get_direction(
             current_position.x,
             current_position.y,

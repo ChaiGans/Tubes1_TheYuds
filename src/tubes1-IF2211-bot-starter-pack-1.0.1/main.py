87a327401a3cc1de7bad9b5e9df3562873cc6e6f
DerwinRustanly <117006907+DerwinRustanly@users.noreply.github.com>
2024-03-02 14:47:43 +0700

    feat: add new bot logic on main

--- src/tubes1-IF2211-bot-starter-pack-1.0.1/main.py
+++ src/tubes1-IF2211-bot-starter-pack-1.0.1/main.py
@@ -12,6 +12,7 @@ from game.logic.shortest_distance import ShortestDistance
 from game.logic.highest_value import HighestValue
 from game.logic.highest_density import HighestDensity
 from game.logic.highest_concentration import HighestConcentration
+from game.logic.nearest_base import NearestBase
 
 
 init()
@@ -22,7 +23,8 @@ CONTROLLERS = {
     "ShortestDistance" : ShortestDistance,
     "HighestValue" : HighestValue,
     "HighestDensity" : HighestDensity,
-    "HighestConcentration" : HighestConcentration
+    "HighestConcentration" : HighestConcentration,
+    "NearestBase": NearestBase,
 }
 
 ###############################################################################
@@ -212,7 +214,7 @@ while True:
 
     # Don't spam the board more than it allows!
     # sleep(move_delay * time_factor)
-    sleep(1)
+    sleep(0.3)
 
 
 ###############################################################################

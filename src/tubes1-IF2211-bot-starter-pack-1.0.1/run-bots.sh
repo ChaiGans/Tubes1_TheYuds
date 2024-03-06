301b01cb00c6965c0df38c45d6b0f985d8f745d4
DerwinRustanly <117006907+DerwinRustanly@users.noreply.github.com>
2024-03-06 11:54:11 +0700

    feat: add readme

--- src/tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.sh
+++ src/tubes1-IF2211-bot-starter-pack-1.0.1/run-bots.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python main.py --logic Random --email=test@email.com --name=stima --password=123456 --team etimo &
-python main.py --logic MyBot --email=test1@email.com --name=stima1 --password=123456 --team etimo &
+python main.py --logic HighestDensity --email=hiden@email.com --name=hiden1 --password=123456 --team etimo &
+python main.py --logic HighestDensity --email=hidenasdasd@email.com --name=hiden2 --password=12345216 --team etimo &
 # python main.py --logic Random --email=test2@email.com --name=stima2 --password=123456 --team etimo &
 # python main.py --logic Random --email=test3@email.com --name=stima3 --password=123456 --team etimo &
